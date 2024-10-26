472041e2dccc6ab182bc75edaba8560eb8432f50
habibunayka <habibunayka@gmail.com>
2024-10-27 05:34:28 +0700

    Redirect

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect('/login');
+});
+
 Route::view('/login', 'auth.login');
 Route::view('/teacher', 'teacher.index');
 Route::view('/teacher/list', 'teacher.list');
